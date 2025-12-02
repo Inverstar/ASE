b1f18274e9686cfa5059bea743349390684c5aad
Inverstar <lnq1208@163.com>
2025-12-02 18:16:24 +0000

    计数器实践

---
+++ src/components/ElmWrapper.svelte
@@ -0,0 +1,18 @@
+<script>
+  import { onMount } from 'svelte';
+  // 导入刚才写的 Elm 文件
+  // 使用 vite-plugin-elm 导入 Elm 模块的默认导出
+  import Counter from '../elm/Counter.elm';
+
+  let node;
+
+  onMount(() => {
+    // 初始化 Elm 应用，挂载到 node 节点上
+    Counter.init({
+      node
+    });
+  });
+</script>
+
+<!-- 这个 div 就是 Elm 渲染的地方 -->
+<div bind:this={node}></div>
